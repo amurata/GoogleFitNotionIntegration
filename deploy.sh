b93c601715a4fe19ff53e4388eb46286dead26cc
amurata <rikuma95@gmail.com>
2025-01-06 19:52:43 +0900

    Update memory allocation for GoogleFitNotionIntegration function

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ gcloud functions deploy GoogleFitNotionIntegration \
     --region=asia-northeast1 \
     --entry-point=handler \
     --timeout=30 \
-    --memory=128Mi \
+    --memory=256Mi \
     --set-env-vars=NOTION_SECRET=${NOTION_SECRET},DATABASE_ID=${DATABASE_ID} \
     --source=./src \
     --project=${GCP_PROJECT}

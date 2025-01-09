a8295fdfc48824af0f545e518a54d20425cc19f4
amurata <rikuma95@gmail.com>
2025-01-09 10:29:58 +0900

    Update trigger_fit.sh to include environment variable loading and publish pubsub topic

--- trigger_fit.sh
+++ trigger_fit.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # 環境変数を読み込む
+echo "Read .env"
 set -a
 source .env
 set +a
 
+
 # Google Fit データ取得をトリガーするスクリプト
+echo "Publish pubsub topic to trigger fit"
 gcloud pubsub topics publish fit --message="trigger" --project=${GCP_PROJECT}
 
 # ログを表示（最新の10件）
